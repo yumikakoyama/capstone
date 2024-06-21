6fc41c908db417246324bc519e3c0c66bdaf23de
YUMIKA KOYAMA <koyumi40@gmail.com>
2024-06-21 16:07:26 -0700

    contact page update

--- reactapp/src/components/ContactForm.js
+++ reactapp/src/components/ContactForm.js
@@ -3,7 +3,14 @@ import"./ContactFormStyles.css"
 function contactForm(){
     return(
         <div className="form-container">
-            <h1>ContactForm</h1>
+            <h1>Send a message to us!</h1>
+            <form>
+                <input placeholder="Name"></input>
+                <input placeholder="Email"></input>
+                <input placeholder="Subject"></input>
+                <textarea placeholder="Message" rows="4"></textarea>
+                <button>Send Message</button>
+            </form>
         </div>
     )
 }

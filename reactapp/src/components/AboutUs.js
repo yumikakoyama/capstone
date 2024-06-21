a0ecd2e780aba5c408f1c307df01873fbeaae0ca
YUMIKA KOYAMA <koyumi40@gmail.com>
2024-06-21 16:02:22 -0700

    about page push

--- reactapp/src/components/AboutUs.js
+++ reactapp/src/components/AboutUs.js
@@ -3,7 +3,14 @@ import "./AboutUsStyles.css"
 function AboutUs (){
     return(
         <div className="about-container">
-            <h1>About us</h1>
+            <h1>Our History</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic explicabo voluptates, quam minima quod odit veritatis vel. Eum itaque, impedit sunt saepe eos tempore quaerat mollitia cumque reiciendis vel libero unde harum ipsum nesciunt corrupti consequuntur sit maxime ipsam rerum.</p>
+
+            <h1>Our Mission</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos ipsam laudantium, natus amet at maiores velit doloremque, sapiente deleniti quaerat beatae laborum. Vitae fugit tempore cupiditate sapiente ipsa placeat ratione, aspernatur voluptatum suscipit dolorum odit autem eaque quia nisi temporibus.</p>
+
+            <h1>Our Value</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate ea harum illum veniam voluptatum autem nobis, eveniet quod quibusdam modi quia neque perspiciatis odio itaque laboriosam fugit. Optio dolorem fugiat nobis accusantium nisi provident, sed autem minima harum ullam quibusdam.</p>
         </div>
     )
 }

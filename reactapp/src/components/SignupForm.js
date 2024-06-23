f41e0fc38f641f36f6c1c144ca7ee120a422970b
YUMIKA KOYAMA <koyumi40@gmail.com>
2024-06-23 13:01:08 -0700

    signup page push

--- reactapp/src/components/SignupForm.js
+++ reactapp/src/components/SignupForm.js
@@ -3,7 +3,17 @@ import"./SignupFormStyles.css"
 function sighupForm(){
     return(
         <div className="form-container">
-            <h1 className="signup">Sign up</h1>
+            <h1 className="signup">Sign up for Trippy!</h1>
+            <p>Create a free account or <a href="/login">login</a></p>
+            <form>
+                <input placeholder="First Name"></input>
+                <input placeholder="Last Name"></input>
+                <input placeholder="Email"></input>
+                <input type="tel" placeholder="Phone Number"></input>
+                <input type="password" placeholder="Password"></input>
+                <input type="password" placeholder="Confirm Password"></input>
+                <button>Register</button>
+            </form>
         </div>
     )
 }

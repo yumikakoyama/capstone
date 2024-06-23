6b6899206e66f079b359596ae0863532173bdbc0
YUMIKA KOYAMA <koyumi40@gmail.com>
2024-06-23 13:03:01 -0700

    login page push

--- reactapp/src/components/LoginForm.js
+++ reactapp/src/components/LoginForm.js
@@ -4,6 +4,11 @@ function loginForm(){
     return(
         <div className="form-container">
             <h1 className="signup">Login</h1>
+            <form>
+                <input placeholder="Email"></input>
+                <input type="password" placeholder="Password"></input>
+                <button>Login</button>
+            </form>
         </div>
     )
 }
